9c67b218f69852178c078fba4d19d9f5527b449e
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2015-12-20 15:26:07 +0530

    Create DragRigidbody.cs

---
+++ DragRigidbody/DragRigidbody.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+
+public class mouseDrag : MonoBehaviour {
+
+	private float distance1=2;
+	private float distance;
+
+	[HideInInspector]
+	public GameObject thePlayer;
+
+	[HideInInspector]
+	public Rigidbody rb;
+
+	private bool isDragged;
+
+	void Awake()
+	{
+		thePlayer = GameObject.Find ("Player");
+		if (thePlayer == null)
+			Debug.Log ("Player Not Found");
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
+
+	void OnMouseDrag()
+	{
+		isDragged = true;
+		thePlayer.GetComponent<Rigidbody> ().freezeRotation = true;
+
+		Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance1);
+		Vector3 objPosition = Camera.main.ScreenToWorldPoint (mousePosition);
+
+		transform.position = objPosition; // storing the position on the game Object's new position
+
+
+	}
+
+	void OnMouseUp()
+	{
+		isDragged = false;
+	}
+
+	void Update()
+	{
+		// Calculating the player position at every frame
+		float playerX = thePlayer.transform.position.x;
+		float playerY = thePlayer.transform.position.y;
+		float playerZ = thePlayer.transform.position.z;
+		// Calculating the gameObject's position at every frame
+		float objX = transform.position.x;
+		float objY = transform.position.y;
+		float objZ = transform.position.z;
+		// Calculating the distance
+		distance = Mathf.Sqrt (Mathf.Pow((playerX-objX),2)+Mathf.Pow((playerY-objY),2)+Mathf.Pow((playerZ-objZ),2));
+
+		rb.freezeRotation = isDragged;
+
+	}
+}
