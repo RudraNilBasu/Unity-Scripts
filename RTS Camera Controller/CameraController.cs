48ca14d48422e0120126849b72ec4a720af8a5b1
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-08-15 17:46:09 +0530

    RTS Camera Controller

---
+++ RTS Camera Controller/CameraController.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+
+public class CameraController : MonoBehaviour {
+
+    public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
+
+    private bool doMovement = true;
+    private float scrollSpeed = 5f;
+    public float minY = 10f;
+    public float maxY = 80f;
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            doMovement = !doMovement;
+        }
+
+        if (doMovement == false) {
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.mousePosition.y >= Screen.height - panBorderThickness ) {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+        if(Input.GetKey(KeyCode.S) || Input.mousePosition.y <=  panBorderThickness) {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.mousePosition.x <= panBorderThickness)
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        float scroll =  Input.GetAxis("Mouse ScrollWheel");
+
+        Vector3 pos = transform.position;
+        pos.y -= scroll * 1000 *  scrollSpeed*Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        transform.position = pos;
+    }
+}
